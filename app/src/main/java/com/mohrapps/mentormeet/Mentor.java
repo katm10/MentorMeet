31ca9e50db06d39d4093628227f5674d4af1cb9b
Katherine <katgm10@gmail.com>
2016-10-16 18:22:26 -0700

    Working on sign in

---
+++ app/src/main/java/com/mohrapps/mentormeet/Mentor.java
@@ -0,0 +1,13 @@
+package com.mohrapps.mentormeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mentor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor);
+    }
+}
